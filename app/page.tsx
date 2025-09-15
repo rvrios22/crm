03db394e390f93dc52a96156ceec0a30544e7885
Rafael Rios <rvrios22@gmail.com>
2025-09-15 06:18:35 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import Image from "next/image";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
+  console.log(tasks)
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
