53544fef74adc894ec0658dbe572e0bcbcaf0497
Rafael Rios <rvrios22@gmail.com>
2025-09-17 15:53:17 -0700

    feat: add user to db after account creation

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -3,15 +3,25 @@ import { NextRequest } from "next/server";
 import { api } from "@/convex/_generated/api";
 import { fetchMutation } from "convex/nextjs";
 
+const findEmail = (primaryEmailId: string, emails: any[]) => {
+  const emailObj = emails.find((email) => email.id === primaryEmailId);
+  return emailObj.email_address;
+};
+
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
-    const newUser = fetchMutation(api.users.mutations.createUser, {
-      name: evt.data.first_name as string,
+    const newUser = await fetchMutation(api.users.mutations.createUser, {
+      name: `${evt.data.first_name} ${evt.data.last_name}`,
       phone: "",
-      email: evt.data.email_address,
+      email: findEmail(
+        evt.data.primary_email_address_id,
+        evt.data.email_addresses
+      ),
       clerk_id: evt.data.id,
+      company_id: null,
     });
+    console.log(newUser);
   } catch (err) {
     console.error("Error verifying webhook:", err);
     return new Response("Error verifying webhook", { status: 400 });
